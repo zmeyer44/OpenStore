d5e56d6beb95d8c369b59f8a9505ca151ff49d59
Claudius <claudius.bot.1@gmail.com>
2026-04-05 21:27:36 -0400

    updating GA set up

--- apps/web/lib/analytics/google-analytics.tsx
+++ apps/web/lib/analytics/google-analytics.tsx
@@ -1,23 +1,6 @@
-import Script from "next/script";
+import { GoogleAnalytics as GA } from "@next/third-parties/google";
 
 export function GoogleAnalytics() {
   if (!process.env.NEXT_PUBLIC_GA_TRACKING_ID) return null;
-  return (
-    <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
-        strategy="lazyOnload"
-      />
-      <Script id="google-analytics" strategy="lazyOnload">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-        `}
-      </Script>
-    </>
-  );
+  return <GA gaId={process.env.NEXT_PUBLIC_GA_TRACKING_ID} />;
 }
